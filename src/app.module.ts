e23de9641d8b622f3fd7375af40e474bd44cc32e
chenwei <fengzhi.chen@tuya.com>
2022-03-10 21:46:32 +0800

    feat: 开发完成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticleModule } from './modules/article/article.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // 使用 TypeORM 配置数据库
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'article',
+      entities: ['dist/modules/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
